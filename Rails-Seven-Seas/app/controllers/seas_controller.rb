b7f26d3022b504cb7f6858b0000bf50e65d79bf9
timtan93 <timtan93@gmail.com>
2019-01-11 15:35:06 +0000

    Working, forms aren't formatted

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,5 +1,38 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  def index
+    @seas = Sea.all
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def new
+    @sea = Sea.new
+  end
+
+  def create
+    @sea = Sea.create(sea_params)
+    redirect_to @sea
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+      @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+    redirect_to @sea
+  end
+
+  def destroy
+    @sea = Sea.find(params[:id])
+    @sea.delete
+    redirect_to action: "index"
+  end
+
 
 
   private
@@ -9,5 +42,7 @@ class SeasController < ApplicationController
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
+
+
 
 end
---
+++ Rails-Seven-Seas/app/models/sea.rb
@@ -0,0 +1,4 @@
+class Sea < ApplicationRecord
+
+
+end
